da0a95dff75da04d5220fd5ed31a9585f87c6c37
Simon Rodriguez <esimonrodrig@gmail.com>
2022-03-24 22:21:07 -0400

    plugins should load before config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,12 @@
+-- Other Lua Config
+require('plugins')
+require('keybinds')
+
 -- Plugin setup
 require('plugins/lualine')
 require('plugins/nvimtree')
 require('plugins/cmp')
 require('plugins/treesitter')
-
--- Other Lua Config
-require('plugins')
-require('keybinds')
 
 local opt = vim.opt
 local cmd = vim.cmd
